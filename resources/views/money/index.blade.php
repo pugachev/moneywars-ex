fb23f1b1b76d8b1e1c03ce73b3b242e85dc397d8
pugachev <pugachev2011@gmail.com>
2024-12-31 16:06:53 +0900

    ヘッダーの文字列を修正

--- resources/views/money/index.blade.php
+++ resources/views/money/index.blade.php
@@ -27,7 +27,7 @@
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-danger sticky-top mb-0">
-        <a class="navbar-brand" href="{{route('money.index')}}">MoneyWars</a>
+        <a class="navbar-brand" href="{{route('money.index')}}">moneywars</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
