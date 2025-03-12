15550d31fe9076b91c6936b778053f12e92ce7b2
pugachev <pugachev2011@gmail.com>
2025-03-12 20:55:58 +0900

    Amazonの月別合計

--- app/Http/Controllers/MoneyController.php
+++ app/Http/Controllers/MoneyController.php
@@ -107,7 +107,6 @@ class MoneyController extends Controller
             'holiday'      => [],
             'monthlyTotal' => $monthlyTotal,
             'itemTotals'   => $itemTotals,
-            'amazonTotal'  => $amazonTotal,
         ]);
     }
 
@@ -117,14 +116,12 @@ class MoneyController extends Controller
             'date'    => 'required|date',
             'amount'  => 'required|numeric',
             'tgtitem' => 'required|numeric|between:1,5',
-            'description' => 'nullable|string|max:255',
         ]);
 
         Spending::create([
             'tgtdate'  => $validated['date'],
             'tgtmoney' => $validated['amount'],
             'tgtitem'  => $validated['tgtitem'],
-            'description' => $validated['description'],
         ]);
 
         return redirect()->route('money.index')
